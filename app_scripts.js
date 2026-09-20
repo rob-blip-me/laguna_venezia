4df2091108359e1392dfda1ceb7a35b53ea67cf5
rob-blip-me <roberto.rnld@gmail.com>
2026-09-20 11:44:13 +0200

    Update app_scripts.js

--- app_scripts.js
+++ app_scripts.js
@@ -231,7 +231,7 @@ const obstacles=[
  lat:45.521730,
  lon:12.425163,
  description:"Possibile presenza di un cavo elettrico sospeso"
-},45.52177592610227, 12.42520645767065
+},
 {
  name:"Attenzione: Bilancia",
  lat:45.521775,
